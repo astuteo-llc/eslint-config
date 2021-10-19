c3071c5d7f932e1ff4cc16c7228f7047bc5c9186
Dustin Walker <me@dustinwalker.com>
2021-10-19 15:57:19 -0500

    FIX: conflict with prettier

--- rules/style.js
+++ rules/style.js
@@ -123,7 +123,7 @@ module.exports = {
 
         // Enforce the location of arrow function bodies with implicit returns
         // https://eslint.org/docs/rules/implicit-arrow-linebreak
-        'implicit-arrow-linebreak': ['error', 'beside'],
+        'implicit-arrow-linebreak': ['error', 'below'],
 
         // this option sets a specific tab width for your code
         // https://eslint.org/docs/rules/indent
